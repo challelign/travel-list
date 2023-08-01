2a8f31638f962f2941f9bbabf7d75ce96ecba20a
Challelign Tilahun <chalie2123@gmail.com>
2023-08-01 09:39:52 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+import { useState } from "react";
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
+function App() {
+	return (
+		<div className="app">
+			<Logo />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Stats.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Stats = () => {
+	return (
+		<footer className="stats">
+			<em>You X items on Your List , and you already packed x (X%)</em>
+		</footer>
+	);
+};
+
+export default Stats;
