b7d5ffa156e45a56ad9bfb1a162e9832d9a99bea
Challelign Tilahun <chalie2123@gmail.com>
2023-08-01 11:06:02 +0300

    create submit form and handle the error while form submit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+	const [description, setDescription] = useState("");
+	const [quantity, setQuantity] = useState(5);
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		alert("you hit me");
+		// console.log(e);
+
+		if (!description || description.trim() === "") return;
+		const newItem = {
+			description,
+			quantity,
+			package: false,
+			id: Date.now(),
+		};
+		console.log(newItem);
+
+		setDescription("");
+		setQuantity(5);
 	};
 	return (
 		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your 😂 trip ?</h3>
-			<select>
+			<select
+				value={quantity}
+				onChange={(e) => {
+					console.log(e.target.value);
+					setQuantity(Number(e.target.value));
+				}}
+			>
 				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
 					<option value={num} key={num}>
 						{num}
 					</option>
 				))}
 			</select>
-			<input type="text" placeholder="Items ..."></input>
+			<input
+				type="text"
+				value={description}
+				onChange={(e) => {
+					console.log(e.target.value);
+					setDescription(e.target.value);
+				}}
+				placeholder="Items ..."
+			></input>
 			<button>Add</button>
 		</form>
 	);
